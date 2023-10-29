4b2ddb007b62f8d86ffa7f15163e6540d5cd2e3c
Javier Pastor <jpastor@cerebelum.net>
2023-10-29 19:35:21 +0100

    New: Remediation Enabled_LocalAdmin.

---
+++ Scripts_Remediations/Enabled_LocalAdmin/Enabled_LocalAdmin_Detecction.ps1
@@ -0,0 +1,11 @@
+﻿$adminName    = (Get-WmiObject -Class Win32_UserAccount | Where-Object { $_.SID -like 'S-1-5-21-*-500' }).Name
+$adminAccount = Get-LocalUser -Name $adminName
+if ($adminAccount.Enabled -eq $false)
+{
+    Write-Host "Local Admin is Disabled"
+    Exit 1
+}
+else
+{
+    Exit 0
+}
---
+++ Scripts_Remediations/Enabled_LocalAdmin/Enabled_LocalAdmin_Remediation_Script.ps1
@@ -0,0 +1,38 @@
+﻿# No compatible con PW 7
+# Valido para el PW que trae el sistema version 5.
+
+$errCode   = 0
+$adminName = ""
+Try {
+    $adminName = (Get-WmiObject -Class Win32_UserAccount | Where-Object { $_.SID -like 'S-1-5-21-*-500' }).Name
+    Write-Host ("UserName Local Admin: {0}" -f $adminName) -ForegroundColor Green
+}
+Catch
+{
+    $errCode = $_.Exception.HResult
+    $errMsg  = $_.Exception.Message
+    Write-Warning -Message ("Failed to enabled user '{0}'. Error ({1}): {2}" -f $adminName, $errCode, $errMsg)
+}
+
+if ($errCode -eq "0")
+{
+    $adminAccount = Get-LocalUser -Name $adminName
+    if ($adminAccount.Enabled -eq $false)
+    {
+        Try {
+            Enable-LocalUser -Name $adminName
+            Write-Host "The local administrator has been enabled."
+        }
+        Catch {
+            $errCode = $_.Exception.HResult
+            $errMsg  = $_.Exception.Message
+            Write-Warning -Message ("Failed to enabled user '{0}'. Error ({1}): {2}" -f $adminName, $errCode, $errMsg)
+        }
+    }
+    else
+    {
+        Write-Host "The local administrator is already enabled."
+    }
+}
+
+Exit $errCode
